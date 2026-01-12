6c3d5777a45d4d916518c54122749e700495cfd2
KarelTestSpecial <karel.test.special@gmail.com>
2026-01-12 10:33:26 +0100

    fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ if (fs.existsSync(pluginPath)) {
 }
 
 export default defineConfig({
-  base: '/karel-portfolio/', 
+  base: '/ath-karel-portfolio/', 
   plugins: [
     react(),
     tailwindcss(),
